89a0d4bec902ea2af8f913f358c861b5813c0852
sonrisas2000 <pau.saez4600@gmail.com>
2021-03-10 12:27:07 +0100

    Webpack with react

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('terser-webpack-plugin');
+const {cleanWebpackPlugin, CleanWebpackPlugin} = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path: path.resolve(__dirname, 'dist' ),
+        filename: 'bundle.js',
+    },
+    resolve:{
+        extensions: ['.js', '.jsx'],
+        alias:{
+            "@components": path.resolve(__dirname, 'src/components/'),
+            "@styles": path.resolve(__dirname, 'src/styles/')
+        }
+    },
+    mode: 'production',
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.html$/,
+                use:[
+                    { loader: 'html-loader' }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+
+    },
+    optimization: {
+        minimizer: true,
+        minimizer:[
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    
+}
